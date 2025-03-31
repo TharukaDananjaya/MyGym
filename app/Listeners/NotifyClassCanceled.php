4bf7d3bce858df1ed2e219a8208d5ff6f1299885
TharukaDananjaya <tharukadananjaya@Tharukas-MacBook-Pro.local>
2025-03-31 10:59:39 +0530

    Added booking canceled notification feature

---
+++ app/Listeners/NotifyClassCanceled.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ClassCanceled;
+use App\Jobs\NotifyClassCanceledJob;
+use App\Mail\ClassCanceledMail;
+use App\Notifications\ClassCanceledNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
+
+class NotifyClassCanceled
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(ClassCanceled $event): void
+    {
+        $members = $event->scheduledClass->members()->get();
+        $className = $event->scheduledClass->classType->name;
+        $classDateTime = $event->scheduledClass->date_time;
+        $details = compact("className", "classDateTime");
+        // $members->each(function($user) use ($details) {
+        //     // Send a mail
+        //     Mail::to($user)->send(new ClassCanceledMail($details),);
+        // });
+        // Notification::send($members, new ClassCanceledNotification($details));
+        NotifyClassCanceledJob::dispatch($members, $details);
+    }
+}
